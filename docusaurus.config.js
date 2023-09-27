f78e1a6f591ee59be02dd37a861f41f62f8dd1fa
codeLearnerrr <jeferson.eiji@icloud.com>
2023-09-27 20:52:13 -0300

    fix: fixed baseUrl

--- docusaurus.config.js
+++ docusaurus.config.js
@@ -14,7 +14,7 @@ const config = {
   url: 'https://codelearnerrr.github.io',
   // Set the /<baseUrl>/ pathname under which your site is served
   // For GitHub pages deployment, it is often '/<projectName>/'
-  baseUrl: '/tcc-senai/',
+  baseUrl: '/',
   trailingSlash: false,
 
   // GitHub pages deployment config.
